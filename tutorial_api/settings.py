ddd4487b00e21defba1373988a0b9528bd0b829c
kalkidan getachew <kalgetachew375@gmail.com>
2025-05-16 13:34:30 +0300

    fix deployment erro

--- tutorial_api/settings.py
+++ tutorial_api/settings.py
@@ -183,7 +183,7 @@ if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
